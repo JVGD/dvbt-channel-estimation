3a8025fdb2ec4b6e97d9a4cab05ec3be92f12a2b
Javier V <j.vargas.g.d@gmail.com>
2018-08-12 13:32:09 +0000

    Generic counter with step

---
+++ cont_N_i_M.vhd
@@ -0,0 +1,72 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.math_real.all;
+
+entity cont_N_i_M is
+	generic( 
+		N : integer := 0; -- counter init number
+		i : integer := 1; -- counter step count
+		M : integer := 10 -- counter end number
+		);
+    port(
+        clk : in std_logic;
+        rst : in std_logic;
+        enable: in std_logic;
+        counter : out std_logic_vector(natural(ceil(log2(real(M+1))))-1 downto 0);
+        cont_ended : out std_logic
+        );
+
+end cont_N_i_M;
+
+architecture behavioral of cont_N_i_M is
+    
+    signal cont : std_logic_vector(natural(ceil(log2(real(M+1))))-1 downto 0);
+    signal p_cont : std_logic_vector(natural(ceil(log2(real(M+1))))-1 downto 0);
+    signal p_cont_ended : std_logic := '0';
+    signal s_cont_ended : std_logic := '0';
+    signal stop_count : std_logic := '0';
+    signal p_stop_count : std_logic := '0';
+    
+begin   
+
+    comb : process(cont, rst, enable)
+    begin
+        if ((rst = '0') and (enable = '1')) then
+            if (stop_count = '0') then
+                p_cont <= std_logic_vector(unsigned(cont) + i);            
+            end if;
+            
+            if (unsigned(cont) = M-i) then
+                p_stop_count <= '1';
+            end if;
+            
+            if (unsigned(cont) >= M) then
+                p_cont_ended <= '1';
+            end if;
+        
+		elsif (rst = '1') then
+			p_cont <= std_logic_vector(to_unsigned(N, p_cont'length));
+        end if;
+        
+    end process comb;
+
+
+    sync : process(rst, clk)
+    begin
+        
+        if (rst = '1') then
+            cont <= std_logic_vector(to_unsigned(N, cont'length));
+        elsif (rising_edge(clk)) then
+            cont <= p_cont;
+            counter <= p_cont;
+            cont_ended <= p_cont_ended;
+            s_cont_ended <= p_cont_ended;
+            stop_count <= p_stop_count;
+        
+        end if;
+    
+    end process sync;
+
+end behavioral;
+
---
+++ tb_cont_N_i_M.vhd
@@ -0,0 +1,91 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.math_real.all;
+
+entity tb_cont_N_i_M is
+end tb_cont_N_i_M;
+
+architecture behavioral of tb_cont_N_i_M is
+
+    -- clkmanager component
+    component clkmanager is
+        generic (
+            clk_period       : time      := 10 ns;  --! period of generated clock
+            rst_active_value : std_logic := '0';    --! reset polarity
+            rst_cycles       : integer   := 10      --! number of cycles that reset will be asserted at the beginning of the simulation
+            );
+        port (
+            endsim : in  std_logic;  --! \c clk stops changing when endsim='1', which effectively stops the simulation
+            clk    : out std_logic;  --! Generated clock
+            rst    : out std_logic   --! Generated reset
+            );
+        end component;
+		
+	-- cont
+	component cont_N_i_M is
+		generic( 
+			N : integer := 0; -- counter init number
+			i : integer := 1; -- counter step count
+			M : integer := 10 -- counter end number
+			);
+		port(
+			clk : in std_logic;
+			rst : in std_logic;
+			enable: in std_logic;
+			counter : out std_logic_vector(natural(ceil(log2(real(M+1))))-1 downto 0);
+			cont_ended : out std_logic
+			);
+		end component;
+		
+		--signals for clk manager
+		signal clk: std_logic;
+		signal rst: std_logic;
+		
+		-- signals for counter
+		signal N : integer := 2;
+		signal i : integer := 2;
+		signal M : integer := 8;
+		signal enable : std_logic := '0';
+		signal counter : std_logic_vector(natural(ceil(log2(real(M+1))))-1 downto 0);
+		signal cont_ended : std_logic;
+		
+
+begin
+
+	-- Clock manager instance
+    uut_clkmanager : clkmanager
+        generic map(
+            clk_period => 10 ns,
+            rst_active_value => '1',
+            rst_cycles => 2
+			)
+        port map (
+            endsim => '0',
+            clk => clk,
+            rst => rst
+            );
+	
+	uut_cont_N_i_M : cont_N_i_M
+		generic map( 
+			N => N,
+			i => i, -- counter step count
+			M => M  -- counter end number
+			)
+		port map(
+			clk => clk,
+			rst => rst,
+			enable => enable,
+			counter => counter,
+			cont_ended => cont_ended
+			);
+	
+	-- stimulus process
+	stim_proc: process
+	begin
+		wait for 90 ns;
+		enable <= '1';
+		wait;
+	end process;
+	
+end behavioral;

3fb9bf0931a0f7e0e7a72e32e21a2a19be3475d7
Javier V <j.vargas.g.d@gmail.com>
2018-09-16 10:27:13 +0000

    Bloque 8 simplified and verified

--- bloque_8.vhd
+++ bloque_8.vhd
@@ -41,25 +41,13 @@ architecture behavioral of bloque_8 is
 			);
 		end component; 
     
-    signal enable	: std_logic := '0';
+    signal enable	: std_logic;
     signal addr 	: std_logic_vector(10 downto 0);
-	signal p_addr 	: std_logic_vector(10 downto 0);
-    signal p_valid	: std_logic := '0';
-	signal p_pilot_txrx_fin : std_logic := '0';
-	
-    signal spilot_rx : complex12;
-	signal p_pilot_rx : complex12;
-	signal spilot_tx_signed : std_logic;
-	signal p_pilot_tx_signed : std_logic;
+    signal p_valid	: std_logic;
+	signal fin : std_logic;
+	signal p_fin : std_logic;
 	
 begin
-
---	-- Wiring input pilot rx (data_symb) to pilot_rx_out
---	pilot_rx.re <= data_symb(23 downto 12);
---	pilot_rx.im <= data_symb(11 downto 0);
---	
---	-- Wiring as well the sign bit to the pilot_tx_signed
---	pilot_tx_signed <= data_pilot(23);
 
 	uut_cont_N_i_M : cont_N_i_M
 		generic map( 
@@ -71,27 +59,19 @@ begin
 			clk => clk,
 			rst => rst,
 			enable => enable,
-			counter => p_addr,
-			cont_ended => p_pilot_txrx_fin
+			counter => addr,
+			cont_ended => p_fin
 			);
 
-    comb : process(rst, enable, symb_ready, pilot_ready, p_pilot_txrx_fin, data_symb, data_pilot)
+    comb : process(rst, symb_ready, pilot_ready, data_symb, data_pilot, addr, p_fin)
     begin
 		-- Enable condition & p_valid condition
-		if (symb_ready = '1') and (pilot_ready = '1') and (p_pilot_txrx_fin = '0') then
-			-- Counter enable conditions
+		if (symb_ready = '1') and (pilot_ready = '1') and (p_fin = '0') then
 			enable <= '1';
 			p_valid <= '1';
-			-- Getting data for outputting
-			p_pilot_rx.re <= data_symb(23 downto 12);
-			p_pilot_rx.im <= data_symb(11 downto 0);
-			p_pilot_tx_signed <= data_pilot(23);
 		else
 			enable <= '0';
 			p_valid <= '0';
-			p_pilot_rx.re <= spilot_rx.re;
-			p_pilot_rx.im <= spilot_rx.im;
-			p_pilot_tx_signed <= spilot_tx_signed ;
 		end if;
 		
     end process comb;
@@ -99,28 +79,22 @@ begin
     sync : process(rst, clk)
     begin
         if (rst = '1') then
-			addr <= (others=>'0');
 			addr_symb <= (others=>'0');
 			addr_pilot <= (others=>'0');
 			valid <= '0';
-			pilot_txrx_fin <= '0';
-			pilot_rx.re <= (others=>'0');
-			pilot_rx.im <= (others=>'0');
-			pilot_tx_signed <= '0';
-			spilot_rx.re <= (others=>'0');
-			spilot_rx.im <= (others=>'0');
-			spilot_tx_signed <= '0';
+			fin <= '0';
         elsif (rising_edge(clk)) then
-			addr <= p_addr;
-			addr_symb <= p_addr;
-			addr_pilot <= p_addr;
+			addr_symb <= addr;
+			addr_pilot <= addr;
 			valid <= p_valid;
-			pilot_txrx_fin <= p_pilot_txrx_fin;
-			pilot_rx <= p_pilot_rx;
-			spilot_rx<= p_pilot_rx;
-			pilot_tx_signed <= p_pilot_tx_signed;
-			spilot_tx_signed <= p_pilot_tx_signed;
+			fin <= p_fin;
         end if;
     end process sync;
+
+	-- Getting data for outputting
+	pilot_rx.re <= data_symb(23 downto 12);
+	pilot_rx.im <= data_symb(11 downto 0);
+	pilot_tx_signed <= data_pilot(23);
+	pilot_txrx_fin <= fin;
     
 end behavioral;
